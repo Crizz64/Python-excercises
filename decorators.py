861c51ca6c75f63a583e98e1f5ddce4ca77705b6
LiveConnect pruebas <crizz@utp.edu.co>
2025-06-01 14:05:09 -0500

    Create decorators.py

---
+++ decorators.py
@@ -0,0 +1,3 @@
+#Functions are a fundamental concept in programming, greatly enhancing the efficiency and flexibility of your code. Up until now, you've focused on defining and calling functions. However, Python offers advanced techniques to make your code even more powerful and adaptable.
+
+#In this lesson, we'll learn about decorators, special functions that modify or enhance other functions.
