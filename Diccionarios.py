493e90858404f5792ee3622940817709d7c6ac73
LiveConnect pruebas <crizz@utp.edu.co>
2025-06-10 17:14:21 -0500

    Create Diccionarios.py

---
+++ Diccionarios.py
@@ -0,0 +1,18 @@
+#Dictionaries are collection types used to store data in key:value pairs, 
+# which are considered as items. They are ideal for organizing data into pairs, 
+# where each piece of data (value) has its unique identifier (key).
+
+#For example
+
+
+car = {
+  "brand": "Audi",
+  "model": "Q5",
+  "year": "2008"
+}
+
+print(car["brand"])
+print(car["model"])
+print(car["year"])
+
+
