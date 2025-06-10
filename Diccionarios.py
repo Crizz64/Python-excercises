73a745427caf494f61965ade6b69e98b557d20fb
LiveConnect pruebas <crizz@utp.edu.co>
2025-06-10 17:22:08 -0500

    Update Diccionarios.py

--- Diccionarios.py
+++ Diccionarios.py
@@ -15,4 +15,44 @@ print(car["brand"])
 print(car["model"])
 print(car["year"])
 
+#ejercicio 1
+# In this exercise, you will create a dictionary to store contact information.
+# The dictionary should contain the name and company of the contact.
+
+contact = {
+  "name": "John",
+  "company": "Google",
+}
+print(contact["company"])
+
+
+
+#ejercicio 2
+
+#You can get all the values and keys of a dictionary using 
+# the values() and keys() functions, respectively.
+
+contact = {
+  "name": "John",
+  "company": "Google",
+}
+info_keys = contact.keys()
+info_values = contact.values()
+
+print(info_keys)
+print(info_values)
+
+
+#ejercicio 3
+
+car = {
+  "brand": "Audi",
+  "model": "Q5"
+}
+info = car.items()
+print(info)
+
+#The items() function returns a list of tuples, where each tuple contains a key-value pair from the dictionary.
+# You can use this to iterate over the key-value pairs in a dictionary.
+
 
