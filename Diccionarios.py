26266d443f4eb6b61c87f8ee358a42bdb0b5d896
LiveConnect pruebas <crizz@utp.edu.co>
2025-06-11 20:26:21 -0500

    Update Diccionarios.py

--- Diccionarios.py
+++ Diccionarios.py
@@ -56,3 +56,106 @@ print(info)
 # You can use this to iterate over the key-value pairs in a dictionary.
 
 
+#ejercicio 4
+
+#You can use keys not only to access values in a dictionary,
+# but also to change them.
+
+car = {
+  "brand": "Audi",
+  "model": "Q5",
+  "year": "2008"
+}
+
+car["year"] = "2010"
+print(car["year"])
+
+
+
+#ejercicio 5
+
+user = {
+  "Name": "Albert",
+  "Age": 29
+}
+user["Age"] = 30
+
+print(user["Age"])
+print(user.items())
+
+
+#The update() function updates the dictionary with the 
+# items from the given argument.
+
+#The argument must be a dictionary with the item you want 
+# to update.
+
+user = {
+  "Name": "Albert",
+  "Age": 29
+}
+
+# argument: dictionary {"Age": 30}
+user.update({"Age": 30})
+
+print(user["Age"])
+print(user.items())
+
+#ejercicio 6
+#The update() function can accept dictionaries with 
+# multiple items.If an item is new, it will be added to the original dictionary.
+
+user = {
+  "Name": "Albert",
+  "Age": 29
+}
+
+# "Surname": "Johnson" will be added
+user.update({"Age": 30, "Surname": "Johnson"})
+
+print(user.items())
+
+
+#ejercicio 7
+#The pop() function removes the item with the specified key 
+# name. It accepts the key of the item you want to remove 
+# as an argument.
+
+user = {
+  "Name": "Albert",
+  "Age": 29
+}
+
+user.pop("Age")
+
+print(user.items())
+
+
+
+#ejercicio 8
+#You can use the in operator to check if a key or a value 
+# occurs in a dictionary.
+
+user = {
+  "Name": "Albert",
+  "Age": 29
+}
+
+print("Name" in user)  # True
+print("Surname" in user)  # False
+print("Albert" in user.values())  # True
+print(30 in user.values())  # False
+
+
+#ejercicio 9
+#You can iterate through a dictionary using a for loop.
+#If you loop through a dictionary, it will return the keys.
+
+car = {
+  "Brand": "Ford",
+  "Model": "Mustang",
+  "Color": "red"
+}
+
+for i in car:
+  print(i)
