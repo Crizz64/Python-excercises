bb502962559b20cba9d2fcf8f3b167fe982d3037
LiveConnect pruebas <crizz@utp.edu.co>
2025-09-17 23:22:06 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -23,3 +23,36 @@ num2 = int(input("Ingrese el valor de su bono: "))
 total = (num1*2)+num2
 
 print(f"Su salario más bonos es de: {total}")
+
+
+
+n = int(input(""))
+
+print(n**2)
+
+
+
+N = int(input())
+
+for i in range(2, N + 1, 2):
+    print(f"{i}^2 = {i ** 2}")
+
+
+
+x = int(input(""))
+y = int(input(""))
+
+if x >= 0 and y >=0:
+    print("Primeiro")
+
+elif x >= 0 and y < 0:
+    print("quarto")
+
+elif x < 0 and y < 0:
+    print("terceiro")
+    
+elif x < 0 and y >= 0:
+    print("segundo")
+
+else:
+    print("No existe")
