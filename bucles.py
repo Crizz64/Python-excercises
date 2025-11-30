869ad7e9982db6df5365e997b3c72536a8f4517a
LiveConnect pruebas <crizz@utp.edu.co>
2025-11-29 23:31:25 -0500

    Update bucles.py

--- bucles.py
+++ bucles.py
@@ -94,7 +94,7 @@ else:
 
 
   
-
+#ejercicio 10
 age = 75
 if age < 18: 
   print("Junior discount")
@@ -103,3 +103,22 @@ elif age >= 75:
 else:
   print("No discount")
 print("Proceed to payment")
+
+
+
+#ejercicio 11
+
+age = 16
+is_student = True
+
+if age < 18:
+  #se ejecuta si la edad es menor de 18
+  if is_student:
+    #se ejecuta si es menor de 18 y también es estudiante
+    print("20% discount")
+  else:
+    #se ejecuta si es menor de 18 y no es estudiante
+    print("10% discount")
+else:
+  #se ejecuta este código si el cliente tiene 18 años o más
+  print("Regular price")
