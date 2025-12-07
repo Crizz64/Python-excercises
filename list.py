265672d3ad1b92d7223fdfb35823f994b5baa31c
LiveConnect pruebas <crizz@utp.edu.co>
2025-12-07 12:30:10 -0500

    Update list.py

---
+++ list.py
@@ -0,0 +1,9 @@
+#ejercicio 1
+products = ["apples", "oranges", "bananas"]
+products[2] =  "lime"
+print(products[2])
+
+#ejercicio 2
+words = ["rise", "sun", "glasses"]
+print(words[1] + words[0])
+
