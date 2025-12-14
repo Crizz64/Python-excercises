bf7be93d76ee4d4e580b9ffa8d0b267dead8f55c
LiveConnect pruebas <crizz@utp.edu.co>
2025-12-14 12:48:10 -0500

    Update list.py

--- list.py
+++ list.py
@@ -7,3 +7,12 @@ print(products[2])
 words = ["rise", "sun", "glasses"]
 print(words[1] + words[0])
 
+
+
+#El rebanado te permite extraer una porción de una lista.
+# Los índices de inicio y fin están separados por dos puntos : 
+#ejercicio 3
+
+animals =["cat", "dog", "bird", "cow"]
+print(animals[1:4])
+
